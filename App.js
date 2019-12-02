91534499a01a2265878f806bcfd0eb0733b72665
James Tang <jytang10@gmail.com>
2019-12-01 21:40:11 -0800

    Styled flatlist items

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image, Dimensions } from 'react-native';
 import axios from 'axios';
 
 class App extends Component {
@@ -25,15 +25,14 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state.data)
     return (
       <View style={styles.container}>
         <FlatList
           data={this.state.data}
           keyExtractor={(item) => item.first_name}
           renderItem={({item, index}) => {
-            console.log(item);
-            <View style={styles.itemView}>
+            return (
+            <View style={[styles.itemView, index % 2 > 0 ? styles.itemOdd : styles.itemEven]}>
               <View style={styles.imgContainer}>
                 <Image source={{uri:item.avatar}} style={styles.imageStyle}></Image>
               </View>
@@ -42,7 +41,7 @@ class App extends Component {
                 <Text numberOfLines={1}>{item.email}</Text>  
               </View>
             </View>
-          }}
+          )}}
         >
         </FlatList>
       </View>
@@ -58,17 +57,44 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   itemView: {
-
+    flex: 1,
+    width: Dimensions.get('window').width,
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    borderStyle: 'solid',
+    borderColor: 'green',
+    borderBottomWidth: 1.5, 
   },
   imgContainer: {
-
+    flex: 0,
+    width: 110,
+    height: 110,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
   },
   imageStyle: {
-
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
   itemInfo: {
-
-  }
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 15,
+  },
+  name: {
+    fontSize: 30,
+    color: 'green',
+    textAlign: 'left',
+  },
+  itemOdd: {
+    backgroundColor: '#bdc3c7',
+  },
+  itemEven: {
+    backgroundColor: '#ecf0f1',
+  },
 });
 
 export default App;
